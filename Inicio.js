5f115baffc7dcc3b8b1d19e2a2c7483bd536ac6d
Your Name <you@example.com>
2025-08-19 15:51:02 -0300

    Se puede cambiar entre paredes

--- Inicio.js
+++ Inicio.js
@@ -1,7 +1,6 @@
 function irAPantalla(url) {
     window.location.href = url;
   }
-  
   document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('StartButton');
     btn.addEventListener('click', () => irAPantalla('Juego.html'));

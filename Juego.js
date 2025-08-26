0b9ed25387a79afb3744c1604684c36460f9d4c8
Your Name <you@example.com>
2025-08-26 14:26:31 -0300

    a

---
+++ Juego.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function()  
+boton1 = document.getElementById("boton1");
+function cambioPared () {
+    console.log("a")
+}
+boton1.addEventListener ("click", cambioPared)
